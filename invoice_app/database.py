96baa87cfe737ba7a55600227cb50b5a47eb2b91
David Gomez <david.13.gomez@gmail.com>
2022-04-02 10:30:25 -0700

    add SQLAlchemy support

---
+++ invoice_app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# DATABASE URL for PostgreSQL or *.db filepath for SQLite
+DATABASE_URL = "sqlite:///./invoice_app.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+# Create the SQLAlchemy engine
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(
+            DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+# Instances will be database sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Used to create ORM models
+Base = declarative_base()
